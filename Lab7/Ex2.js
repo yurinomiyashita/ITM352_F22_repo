0463b907fedb61ea97d1ff41a90bc326c9a6712d
yurinomiyashita <yurinom@hawaii.edu>
2022-09-27 14:04:26 -1000

    xx

--- Lab7/Ex2.js
+++ Lab7/Ex2.js
@@ -1,13 +1,33 @@
-require("./products_data.js");
-var num_products = 5;
-var prod_num=0;
+require("./product_data.js");
 
-while( prod_num ++ < num_products){
-    console.log(prod_num);
-    if( (prod_num>num_products*.75) || (prod_num<num_products*.25) ){
-        console.log(`${eval('name' + prod_num)} is sold out!`);
-        continue;
-    }
 
+for(product_index = 1; eval("typeof name"+product_index) != 'undefined' ; product_index++) {
+
+    console.log(`${product_index}. ${eval('name' + product_index)}`);
+    
 }
-for(prod_num=0);
+console.log("That's all we have!");
+
+document.write(`
+<table border="1">
+    <tr><th>Number</th><th>Item</th></tr> 
+    <tr><td>1</td><td>HTC</td></tr>
+    <tr><td>2</td><td>Apple</td></tr>
+    <tr><td>3</td><td>Nokia</td></tr>
+    <tr><td>4</td><td>Samsung</td></tr>
+    <tr><td>5</td><td>Blackberry</td></tr>
+</table>
+`);
+
+//<tr><td>${prod_num}</td><td>${eval("name"+prod_num)}</td></tr>
+/* this is for table one
+*/
+//<script src="./products_data.js"></script>
+/* while(prod_num++ <num_products){ 
+    document.write(`
+    <tr><td>${prod_num}</td><td>${eval("name"+prod_num)}</td></tr>
+    
+    `);
+}
+</script>
+*/
