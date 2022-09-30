d4384d7a80c31a8c3f8af47af618d1d9b9819b81
yurinomiyashita <yurinom@hawaii.edu>
2022-09-29 23:05:49 -1000

    xx

--- WODs/SmartPhoneProducts1_Loop/products_data.js
+++ WODs/SmartPhoneProducts1_Loop/products_data.js
@@ -1,19 +1,24 @@
-var name1 = "HTC";
+        //product 1
+        var name1 = "HTC";
         var price1 = 40.00;
         var image1 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg";
    
+        //product 2
         var name2 = "Apple";
         var price2 = 75.00;
         var image2 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg";
    
+        //product 3
         var name3 = "Nokia";
         var price3 = 35.00;
         var image3 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg";
    
+        //product 4
         var name4 = "Samsung";
         var price4 = 45.00;
         var image4 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg";
-   
+        
+        //product 5
         var name5 = "Blackberry";
         var price5 = 10.00;
         var image5 = "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg";
