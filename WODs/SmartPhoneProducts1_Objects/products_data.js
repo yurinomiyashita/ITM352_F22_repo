74b815a6e34c703a23d1a04a5eded3b8ad121c58
yurinomiyashita <yurinom@hawaii.edu>
2022-10-02 23:40:48 -1000

    xx

--- WODs/SmartPhoneProducts1_Objects/products_data.js
+++ WODs/SmartPhoneProducts1_Objects/products_data.js
@@ -1,39 +1,38 @@
         //product 1
         var product1={
-         'name': "HTC",
+         'name' : "HTC",
          'price' : 40.00,
          'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
-        };
-        
+         };
         //product 2
         var product2={
-        'name' : "Apple",
-        'price' : 75.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+         'name' : "Apple",
+         'price' : 75.00,
+         'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
         };
 
         //product 3
         var product3={
-        'name': "Nokia",
-        'price' : 5.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+         'name' : "Nokia",
+         'price' : 5.00,
+         'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
         };
 
         //product 4
         var product4={
-        'name': "Samsung",
-        'price' : 5.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+         'name' : "Samsung",
+         'price' : 5.00,
+         'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
         };
 
         //product 5
         var product5={
-        'name' : "Blackberry",
-        'price' : 10.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+         'name' : "Blackberry",
+         'price' : 10.00,
+         'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
         };
-        
-        //product array 
-       var products_array=[product1,product2,product3,product4,product5];
 
-console.log("Product lables loaded...");
+        //product array 
+        var product_array = [product1, product2, product3, product4, product5];
+
+console.log("Product iables loaded...");
