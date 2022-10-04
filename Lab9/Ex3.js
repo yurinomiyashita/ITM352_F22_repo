ece6dfd89897d1b322bac5e4cbed377ca20f8ded
yurinomiyashita <yurinom@hawaii.edu>
2022-10-04 11:48:34 -1000

    xx

--- Lab9/Ex3.js
+++ Lab9/Ex3.js
@@ -1,2 +1,7 @@
-attributes  =  "Yurino;20;20.5;-19.5" 
+var attributes  =  "Yurino;20;20.5;-19.5" 
 var pieces = attributes.split(':');
+for (let part in pieces){
+    console.log(`${pieces[part]} is type ${typeof part}`);
+}
+console.log(pieces.join(',')
+);
---
+++ Lab9/Ex4.js
@@ -0,0 +1,18 @@
+console.log
+
+//function isNonNegInt(q){}
+var isNonNegInt = function (q){
+errors = []; // assume no errors at first
+if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+console.log(errors.join(','));
+return errors;
+}
+
+var attributes  =  "Yurino;20;20.5;-19.5" 
+var pieces = attributes.split(':');
+for (let part in pieces){
+    console.log(`${pieces[part]} is type ${typeof part} ${isNonNegInt(pieces[part]).join(',')}`);
+    isNonNegInt (pieces[part]);
+}
