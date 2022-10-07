4141abdde5cda52004cd66ce6b57bda65e62efd2
yurinomiyashita <yurinom@hawaii.edu>
2022-10-07 03:11:55 -1000

    xx

---
+++ Lab10/Ex1.js
@@ -0,0 +1,24 @@
+// 12/31/2001
+
+var year = 2001;
+var month = 12; 
+var day= 31; 
+
+step1 = 01;
+step2 = parseInt(01/4);
+step3= step1+step2;
+step4= 5; 
+step6= step3+step4; 
+step7= step6+31; 
+step8= step7;
+step9= step8-1; 
+step10= step9%7;
+
+result= 'monday'; 
+
+ 
+console.log(step2); 
+console.log(step3); 
+console.log(step10); 
+
+ 
