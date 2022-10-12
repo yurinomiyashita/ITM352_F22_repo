d20354e71b1459a400682d3507eff23662ae3768
yurinomiyashita <yurinom@hawaii.edu>
2022-10-11 14:07:06 -1000

    xx

--- WODs/Invoice3/products_data.js
+++ WODs/Invoice3/products_data.js
@@ -1,39 +1,34 @@
-        //product 1
-        var product1={
+        //item 1
+        var item1={
          'name': "HTC",
          'price' : 40.00,
-         'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
         };
         
-        //product 2
-        var product2={
+        //item 2
+        var item2={
         'name' : "Apple",
         'price' : 75.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
         };
 
-        //product 3
-        var product3={
+        //item 3
+        var item3={
         'name': "Nokia",
         'price' : 5.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
         };
 
-        //product 4
-        var product4={
+        //item 4
+        var item4={
         'name': "Samsung",
         'price' : 5.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
         };
 
-        //product 5
-        var product5={
+        //item 5
+        var item5={
         'name' : "Blackberry",
         'price' : 10.00,
-        'image' : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
         };
         
         //product array 
-       var products_array=[product1,product2,product3,product4,product5];
-
+       var item_array=[item1,item2,item3,item4,item5];
+       quantities_array=[1,2,1,1,0]
 console.log("Product lables loaded...");
