a5b022d1a4089a4ebdc982de28adfbd528302ad6
yurinomiyashita <yurinom@hawaii.edu>
2022-10-13 00:21:38 -1000

    xx

--- Lab10/Ex2.js
+++
@@ -1,7 +0,0 @@
-<span id="firstname">Yurino</span>, <span id="lastname">Miyashita</span>
-<br>
-<input type="button" value (swap)
-    onclick="
-    firstname.innerHTML = lastname.innerHTML;
-    lastname.innerHTML = firstname.innerHTML;
-    ">
