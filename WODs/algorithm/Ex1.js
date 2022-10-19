f0f5de626cf5f4cf1679a6da8de87c02f02edbc6
yurinomiyashita <yurinom@hawaii.edu>
2022-10-18 14:35:44 -1000

    xx

--- WODs/algorithm/Ex1.js
+++ WODs/algorithm/Ex1.js
@@ -12,34 +12,50 @@
 */
 
 // setting up the date
-var date=31;
+var day=31;
 var month= "December";
 var year= 2001;
+
+const monthkey = {
+    January:0,
+    February:3,
+    March:2,
+    April:5,
+    May:0,
+    June:3,
+    July:5,
+    August:1,
+    September:4,
+    October:6,
+    November:2,
+    December:4,
+};
 
 // step 1 is the year 
 step1= year; 
 
 //step 2 devide step 1 by 4 drop the remainder add to step 1
-step2= parseInt(year/4);
-remiander1= year%4;
+step2 = parseInt(year/4)+year;
 
 //step 3: devide step 1 by 100 drop the remainder subtract this from step 2
-step3= parseInt(step1/100);
+step3 = step2-parseInt(year/100);
+
 //remainder2= step2%100;
 //step=parseInt(step2-remiander2);
 
 // step 4 divide step 1 by 400 drop the remainder add to step 3
-step4= parseInt(year/400);
-remiander3=
+step4 = parseInt(year/400)+step3;
 
 //step5 add the day to step 4
-
+step5 = day+step4; 
 
 //step6 find the month key in the tale and add this to step 5 
+step6 = monthkey[month] + step5;
 
 // step 7 divide step 6 by  7 keep only the remainder 
+step7 = step6%7;
 
 // day of week resulty 
+const daysarray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-console.log(step1, step2,step3);
-console.log(remainder1);
+console.log(month, day, year, daysarray[step7]); 
