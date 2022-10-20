a38597c8f866073dd7358f82ec6b573f3dfaad6a
yurinomiyashita <yurinom@hawaii.edu>
2022-10-20 11:46:39 -1000

    xx

---
+++ Lab12/info_server_Ex2c.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+app.all('*', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path + request.query.toString());
+});
+
+app.get('/test', function (request, response, next) {
+    response.send(request.method + ' to path ' + request.path + request.query.toString());
+});
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
