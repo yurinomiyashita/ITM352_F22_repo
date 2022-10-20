8753bd883bce039a73dd63a52a551f0f8bfa0692
yurinomiyashita <yurinom@hawaii.edu>
2022-10-19 23:37:52 -1000

    xx

--- WODs/Invoice3/products_data.js
+++ WODs/Invoice3/products_data.js
@@ -3,29 +3,34 @@
         var product=[
         product1={
          'name' : "HTC",
+         'quantity':quantities[0],
          'price' : 40.00,
          },
         //product 2
         product2={
          'name' : "Apple",
+         'quantity':quantities[1],
          'price' : 75.00,
         },
 
         //product 3
         product3={
          'name' : "Nokia",
+         'quantity':quantities[2],
          'price' : 5.00,
         },
 
         //product 4
         product4={
          'name' : "Samsung",
+         'quantity':quantities[3],
          'price' : 5.00,
         },
 
         //product 5
         product5={
          'name' : "Blackberry",
+         'quantity':quantities[4],
          'price' : 10.00,
         },
         ]
