778f44ca0ce617b103035e81195b2dcd73af3b90
yurinomiyashita <yurinom@hawaii.edu>
2022-10-21 10:31:41 -1000

    aaaaaa

--- WODs/Invoice3/products_data.js
+++ WODs/Invoice3/products_data.js
@@ -1,41 +1,48 @@
-        var quantities=[0.4,-1,"cat",5,4]; 
-        var product=[
-        //product 1
-        product1={
-         name : "HTC",
-         price : 40.00,
-         quantity: quantities[0],
-         },
-        //product 2
-        product2={
-         name : "Apple",
-         price : 75.00,
-         quantity: quantities[1]
-        },
+//quantities array 
+var quantities =[5,0.3, "cat", -1,4]
 
-        //product 3
-        product3={
-         name : "Nokia",
-         price : 5.00,
-         quantity: quantities[2]
-        },
+// products array 
+var product=[
+//product 1
+product1 = {
+        name: "HTC",
+        price: 40.00,
+        quanity : quantities[0],
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+},
+//product 2
+product2 = {
+        name: "Apple",
+        price: 75.00,
+        quanity : quantities[1],
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+},
 
-        //product 4
-        product4={
-         name : "Samsung",
-         price : 5.00,
-         quantity: quantities[3]
-        },
+//product 3
+product3 = {
+        name: "Nokia",
+        price: 5.00,
+        quanity : quantities[2],
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+},
 
-        //product 5
-        product5={
-         name : "Blackberry",
-         price : 10.00,
-         quantity: quantities[4]
-        },
-        ]
-        //product array 
-        var 
-        var product_array = [product1, product2, product3, product4, product5];
+//product 4
+product4 = {
+        name: "Samsung",
+        price: 5.00,
+        quanity : quantities[3],
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+},
+
+//product 5
+product5 = {
+        name: "Blackberry",
+        price: 10.00,
+        quanity : quantities[4],
+        image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+},
+]
+//product array 
+var product_array = [product1, product2, product3, product4, product5];
 
 console.log("Product iables loaded...");
