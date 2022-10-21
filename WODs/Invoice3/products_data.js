f4cfc1309d6e8f9cedebb5141d293361cc21c9e5
yurinomiyashita <yurinom@hawaii.edu>
2022-10-20 21:51:14 -1000

    xx

--- WODs/Invoice3/products_data.js
+++ WODs/Invoice3/products_data.js
@@ -1,42 +1,41 @@
-        var quantities= [5, 4, "cat", 2.4, -1];
-        //product 1
+        var quantities=[0.4,-1,"cat",5,4]; 
         var product=[
+        //product 1
         product1={
-         'name' : "HTC",
-         'quantity':quantities[0],
-         'price' : 40.00,
+         name : "HTC",
+         price : 40.00,
+         quantity: quantities[0],
          },
         //product 2
         product2={
-         'name' : "Apple",
-         'quantity':quantities[1],
-         'price' : 75.00,
+         name : "Apple",
+         price : 75.00,
+         quantity: quantities[1]
         },
 
         //product 3
         product3={
-         'name' : "Nokia",
-         'quantity':quantities[2],
-         'price' : 5.00,
+         name : "Nokia",
+         price : 5.00,
+         quantity: quantities[2]
         },
 
         //product 4
         product4={
-         'name' : "Samsung",
-         'quantity':quantities[3],
-         'price' : 5.00,
+         name : "Samsung",
+         price : 5.00,
+         quantity: quantities[3]
         },
 
         //product 5
         product5={
-         'name' : "Blackberry",
-         'quantity':quantities[4],
-         'price' : 10.00,
+         name : "Blackberry",
+         price : 10.00,
+         quantity: quantities[4]
         },
         ]
-
-        var num_prod = 5;
         //product array 
+        var 
         var product_array = [product1, product2, product3, product4, product5];
-        var qty=[0,1,2,3,4]
-        console.log("Product iables loaded...");
+
+console.log("Product iables loaded...");
