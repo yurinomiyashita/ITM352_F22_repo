066c224c7719527e6736158eb8d0b2b5bb6a2a80
yurinomiyashita <yurinom@hawaii.edu>
2022-10-24 22:31:04 -1000

    xx

---
+++ Miyashita_Yurino_Assignment1/products_data.js
@@ -0,0 +1,56 @@
+//these are items for Yurino's Bookstore 
+//IR4 If the purchase is invalid (see three conditions given in the instructions above), change the purchase button text from “Purchase” to text that indicates why the purchase is invaliud e.g. “Please Select Some Items to Purchase”
+
+//quantities array 
+var quantities =[5,0.3, "cat", -1,4]
+
+// products array 
+var product=[
+//product 1
+product1 = {
+        name: "Atomic Habits",
+        author:"James Clear",
+        price: 11.98,
+        quantity_available:5,
+        image: "AtomicHabits.jpg",
+},
+//product 2
+product2 = {
+        name: "Rich Dad Poor Dad",
+        author:"Robert T Kiyosaki",
+        price: 7.68,
+        quantity_available:5,
+        image: "RichDadPoorDad.jpg",
+},
+
+//product 3
+product3 = {
+        name: "Crazy Rich Asian",
+        author:"Kevin Kwan",
+        price: 9.99,
+        quantity_available:5,
+        image: "CrazyRichAsian.jpg",
+},
+
+//product 4
+product4 = {
+        name: "Talking to Strangers",
+        author:"Malcolm Gladwell",
+        price: 9.65,
+        quantity_available:5,
+        image: "TalkingtoStrangers.jpg",
+},
+
+//product 5
+product5 = {
+        name: "Seven Husbands of Evelyn Hugo",
+        author:"Taylor Jekkins Reid",
+        price: 21.92,
+        quantity_available:5,
+        image: "SevenHusband.jpg",
+},
+]
+//product array 
+var product_array = [product1, product2, product3, product4, product5];
+
+console.log("Product iables loaded...");
