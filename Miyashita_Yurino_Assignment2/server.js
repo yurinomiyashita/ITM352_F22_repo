b82bf9b18db88eda8bf587af0877f41c5e080bca
yurinomiyashita <yurinom@hawaii.edu>
2022-11-06 16:38:55 -1000

    xxx

--- Miyashita_Yurino_Assignment2/server.js
+++ Miyashita_Yurino_Assignment2/server.js
@@ -1,37 +1,34 @@
 //created by Yurino Miyashita 
 //modified Blake Saari's server.js 
-
 // Load Express Package
-    var express = require('express');
-    var app = express();
-
+var express = require('express');
+var app = express();
 // Load Body-Parser Package
-    var parser = require("body-parser");
-
+var parser = require("body-parser");
 // Load QueryString Package
-    const qs = require('querystring');
-
+const qs = require('querystring');
 // Get Body
-    app.use(parser.urlencoded({
-  extended: true}));
-
+app.use(parser.urlencoded({
+  extended: true
+}));
 // Monitor all requests
-    app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
-    next();
-    });
+app.all('*', function (request, response, next) {
+  console.log(request.method + ' to ' + request.path);
+  next();
+});
 
 // Route all other GET requests to files in public 
-    app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 // Load Product Data   
-    var products = require(__dirname + '/products.json');
-
+var products = require(__dirname + '/products.json');
 // Initialize Quantity
-    products.forEach((prod, i) => {
-        prod.quantity_available = products[i].quantity_available
-    })
+products.forEach((prod, i) => {
+  prod.quantity_available = products[i].quantity_available
+})
 
+//created by Yurino Miyashita 
+//modified Blake Saari's server.js 
 //determine if there is error in quantity text box.
 //copied from invoice.html in store 1 direcotry and modified 
 function notAPosInt(arrayElement, returnErrors = false) {
@@ -39,15 +36,22 @@ function notAPosInt(arrayElement, returnErrors = false) {
   if (arrayElement == '') {
     arrayElement = 0;
   } 
-
-//assume there was no entry on texbox
+  //assume there was no entry on texbox
   if (Number(arrayElement) != arrayElement) errors.push('Not a number!'); // Check if string is a number value
   if (arrayElement < 0) errors.push('Negative value!'); // Check if it is non-negative
   if (parseInt(arrayElement) != arrayElement) errors.push('Not an integer!'); // Check that it is an integer
   return (returnErrors ? errors : (errors.length == 0))
 }
 
-// Routing taking json file    
+// // Determines entry in textbox
+// function checkQuantityTextbox(qtyTextbox) {
+//   errs = isNonNegInt(qtyTextbox.value, true);
+//   if (errs.length == 0) errs = ['Want to purchase: '];
+//   if (qtyTextbox.value.trim() == '') errs = ['Type desired quantity: '];
+//   document.getElementById(qtyTextbox.name + '_label').innerHTML = errs.join('<font color="red">, </font>');
+// };
+
+// Routing 
 app.get("/products.json", function (request, response, next) {
   response.type('.js');
   var products_str = `var products = ${JSON.stringify(products)};`;
@@ -55,8 +59,9 @@ app.get("/products.json", function (request, response, next) {
 });
 
 // Process purchase request (validate quantities, check quantity available)
+// I changed from 'quantity_' + i to 'quantity' + i +'_label'
 app.post("/purchase", function (request, response, next) {
-  console.log(request.body);                  //getting request from invoice.html body
+  console.log(request.body); //getting request from invoice.html body
   var quantities = request.body['quantity']; //assigning value to quantities as quantity entred in store.html textbox
   var errors = {};
   var available_quantity = false;
@@ -73,17 +78,17 @@ app.post("/purchase", function (request, response, next) {
     }
   }
 
-  //taking quantity entered to display in invoice
-  let quantity_object = {   
+  //
+  let quantity_object = {
     "quantity": JSON.stringify(quantities)
-  };                                                                            //creating string by quantity_object
+  }; //creating string by quantity_object
   console.log(Object.keys(errors));
-  if (Object.keys(errors).length == 0) {                                            //no errors, 
-    for (i in quantities) {                                                      //remove purchase quantity from inventory
+  if (Object.keys(errors).length == 0) { //no errors, 
+    for (i in quantities) { //remove purchase quantity from inventory
       products[i].quantity_available -= Number(quantities[i]);
-    }                                                                            //sends invoice with quantity with quary string
-    response.redirect('./invoice.html?' + qs.stringify(quantity_object));       //inserting value as quary string to invoice.html table 
-  } else {                                                                      //with errors, redirect to store.html  
+    } //sends invoice with quantity with quary string
+    response.redirect('./invoice.html?' + qs.stringify(quantity_object)); //inserting value as quary string to invoice.html table 
+  } else { //with errors, redirect to store.html  
     let errors_obj = {
       "errors": JSON.stringify(errors)
     };
